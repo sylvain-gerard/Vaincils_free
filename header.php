8f3b8e6960b82a56325137acd2dc70caca79b42f
Gerard <sylvaingerard4@hotmail.com>
2018-02-07 23:40:51 +0100

    install

---
+++ header.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <!--Import Google Icon Font-->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
+    <!--Import materialize.css-->
+    <link type="text/css" rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.css" media="screen,projection" />
+    <!--Let browser know website is optimized for mobile-->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link href="css/style.css" rel="stylesheet" type="text/css" />
+</head>
+
+<body>
+    <header>
+        <nav class="nav-extended blue grey">
+            <div class="nav-wrapper">
+                <span class="nav-perso">Vaincils Web Site</span>
+                <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
+                <ul id="nav-mobile" class="right hide-on-med-and-down">
+                    <li><a href="dist/index.html">Angular</a></li>
+                    <li><a href="typescript.html">TypeScript</a></li>
+                    <li><a href="java.html">Java</a></li>
+                    <li><a href="/admin/admin.php">Admin</a></li>
+                </ul>
+                <ul class="side-nav" id="mobile-demo">
+                    <li><a href="dist/index.html">Angular</a></li>
+                    <li><a href="typescript.html">TypeScript</a></li>
+                    <li><a href="java.html">Java</a></li>
+                    <li><a href="/admin/admin.php">Admin</a></li>
+                </ul>
+            </div>
+        </nav>
+    </header>
